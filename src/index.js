a9dff5ea0d34ccf5cdd3ff53f1551a62bd7b7982
Brian <wabwenib66@gmail.com>
2023-01-21 17:24:29 +0300

    Modified Home Page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import { AnimatePresence } from "framer-motion";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AnimatePresence>
-      <App />
-    </AnimatePresence>
+    <App />
   </React.StrictMode>
 );
